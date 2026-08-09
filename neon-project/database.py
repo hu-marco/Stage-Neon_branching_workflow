0fb83484372e730b9c824ff8b5d80bed058b8fe8
“Marco <marco.hu426@edu.unito.it>
2026-08-09 22:01:44 +0200

    creato i modelli

--- neon-project/database.py
+++ neon-project/database.py
@@ -4,6 +4,22 @@ import os
 import psycopg2
 from psycopg2.extras import execute_values
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
+
+Base = declarative_base()
+
 
 
 #create_table
---
+++ neon-project/models.py
@@ -0,0 +1,67 @@
+from sqlalchemy import Column, Integer, Numeric, String, ForeignKey , DateTime
+from app.database import Base
+
+class User(Base):
+
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String)
+    
+
+class Product(Base):
+
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    price= Column(Numeric)
+    stock = Column(Integer)
+
+    coupon_code = Column(String, nullable=True)
+
+class Order(Base):
+
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    created_at = Column(DateTime)
+    total = Column(Numeric)
+
+    coupon_code = Column(String, nullable=True)
+
+
+class Order_item(Base):
+
+    __tablename__ = "order_items"
+
+    id = Column(Integer, primary_key=True)
+    order_id = Column(Integer, ForeignKey("orders.id"))
+    product_id = Column(Integer, ForeignKey("products.id"))
+    quantity = Column(Integer)
+
+class Category(Base):
+
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+  
+class Category(Base):
+
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    city = Column(String)
+    street = Column(String)
+
+
+class Coupon(Base):
+
+    __tablename__ = "coupons"
+    id = Column(Integer, primary_key=True)
+    code = Column(String, unique=True)
+    discount = Column(Integer)
